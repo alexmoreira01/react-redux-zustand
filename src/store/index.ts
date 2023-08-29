9cdb40e15c6768a0c20b946cbf2a5d9ce54c65ce
Alex Moreira <dev02@neokoros.com>
2023-08-29 17:21:35 -0300

    feat: Implements functions with redux in app

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,15 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 import { useSelector, TypedUseSelectorHook } from "react-redux";
+import { player } from "./slices/player";
 
 // Estado Global
 export const store = configureStore({
   // Objeto com quais são as fatias -- Separar Store
-  reducer: {}
+  reducer: {
+    player: player
+    
+  },
+  
 })
 
 export type RootState = ReturnType<typeof store.getState> // getState retorna todo o estado, com o ReturnType ele devolve o retorno da função e typeof retorna o formato
