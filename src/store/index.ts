0ecae2adce73e5252c3cfd56dcc25a2b054b9987
Alex Moreira <dev02@neokoros.com>
2023-08-30 10:34:44 -0300

    feat: Create Api and get with Async Thunks

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,5 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
-import { useSelector, TypedUseSelectorHook } from "react-redux";
+import { useSelector, TypedUseSelectorHook, useDispatch } from "react-redux";
 import { player } from "./slices/player";
 
 // Estado Global
@@ -13,4 +13,7 @@ export const store = configureStore({
 })
 
 export type RootState = ReturnType<typeof store.getState> // getState retorna todo o estado, com o ReturnType ele devolve o retorno da função e typeof retorna o formato
+export type AppDispatch = typeof store.dispatch
+
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector // Buscar informações do estado - useSelector
+export const useAppDispatch: () => AppDispatch = useDispatch
