2eddfd8f76dffa53a6ca014b6be6f89359ca00f5
Alex Moreira <dev02@neokoros.com>
2023-08-30 11:53:19 -0300

    feat: Migrate redux fot zustand

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,22 +1,24 @@
 import ReactPlayer from 'react-player'
-
-import { next, useCurrentLesson } from '../store/slices/player';
-import { useAppDispatch, useAppSelector } from '../store';
 import { Loader } from "lucide-react";
+import { useCurrentLesson, useStore } from '../zustand-store';
 
 export function Video() {
-  const dispatch = useAppDispatch()
   const { currentLesson } = useCurrentLesson()
-  const isCurrentLoading = useAppSelector(state => state.player.isLoading)
+  const { isLoading, next } = useStore(store => {
+    return {
+      isLoading: store.isLoading,
+      next: store.next
+    }
+  })
 
   function handlePlayNext() {
-    dispatch(next())
+    next()
   }
 
 
   return (
     <div className="w-full bg-zinc-950 aspect-video">
-      {isCurrentLoading ? (
+      {isLoading ? (
         <div className="flex h-full items-center justify-center">
           <Loader className="w-6 h-6 text-zinc-400 animate-spin" />
         </div>
